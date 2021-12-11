1835d2b9b926906b0c6325530931337aecebb5f9
Leszek Słowikowski <lslowikowski@gmail.com>
2021-12-11 15:19:21 +0100

    ver 002 - dodano opisy techniczne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,23 @@ package com.company;
 
 import java.util.Scanner;
 
+/********************************************************
+ * nazwa klasy: Main
+ * parametry wejściowe: brak
+ * wartość zwracana: brak
+ * opis: Główna klasa uruchomieniowa programu konsolowego
+ * autor: 78901234567
+ * ****************************************************/
 public class Main {
     final static int LICZBA_ELEMENTOW = 10;
     static int[] tablica = new int[LICZBA_ELEMENTOW];
+    /********************************************************
+     * nazwa funkcji: main
+     * parametry wejściowe: args - parametry przekazywane podczas uruchomienia
+     * wartość zwracana: brak
+     * opis: Główna metoda uruchomieniowa
+     * autor: 78901234567
+     * ****************************************************/
     public static void main(String[] args) {
         Scanner daneWejsciowe = new Scanner(System.in);
         for (int i = 0; i <LICZBA_ELEMENTOW ; i++) {
--- src/com/company/SortowanieKubelkowe.java
+++ src/com/company/SortowanieKubelkowe.java
@@ -2,27 +2,55 @@ package com.company;
 
 import java.util.Arrays;
 
+/********************************************************
+ * nazwa klasy: SortowanieKubelkowe
+ * parametry wejściowe: brak
+ * wartość zwracana: brak
+ * opis: Klasa sortująca tablicę metodą kubełkową
+ * autor: 78901234567
+ * ****************************************************/
 public class SortowanieKubelkowe {
     private int[] tablica;
     private int[] kubelki;
     private int[] posortowana;
 
+    /********************************************************
+     * nazwa funkcji: SortowanieKubelkowe
+     * parametry wejściowe: tablica - tablica do posortowania
+     * wartość zwracana: brak
+     * opis: Konstruktor klasy SortowanieKubelkowe
+     * autor: 78901234567
+     * ****************************************************/
     public SortowanieKubelkowe(int[] tablica) {
         this.tablica = tablica;
         this.posortowana = new int[tablica.length];
     }
 
-    private int znajdzNajwiekszy(){
+    /********************************************************
+     * nazwa funkcji: znajdzNajwiekszy
+     * parametry wejściowe: brak
+     * wartość zwracana: największy element w tablicy
+     * opis: Znajduje największy element w tablicy
+     * autor: 78901234567
+     * ****************************************************/
+    private int znajdzNajwiekszy() {
         int najwiekszy = 0;
-        for (int i = 0; i < tablica.length ; i++) {
-            if(najwiekszy < tablica[i]){
+        for (int i = 0; i < tablica.length; i++) {
+            if (najwiekszy < tablica[i]) {
                 najwiekszy = tablica[i];
             }
         }
         return najwiekszy;
     }
 
-    public void sortuj(){
+    /********************************************************
+     * nazwa funkcji: sortuj
+     * parametry wejściowe: brak
+     * wartość zwracana: brak
+     * opis: sortuje tablicę metodą kubełkową
+     * autor: 78901234567
+     * ****************************************************/
+    public void sortuj() {
         int iloscKubelkow = znajdzNajwiekszy() + 1;
         kubelki = new int[iloscKubelkow];
         for (int i = 0; i < tablica.length; i++) {
@@ -31,17 +59,24 @@ public class SortowanieKubelkowe {
             kubelki[wartosc] += 1;
         }
         int licznik = 0;
-        for (int i = 0; i < kubelki.length ; i++) {
+        for (int i = 0; i < kubelki.length; i++) {
             int ileRazy = kubelki[i];
-            for (int j = 0; j <ileRazy ; j++) {
+            for (int j = 0; j < ileRazy; j++) {
                 posortowana[licznik] = i;
-                licznik ++;
+                licznik++;
             }
         }
     }
 
+    /********************************************************
+     * nazwa funkcji: toString
+     * parametry wejściowe: brak
+     * wartość zwracana: napis zawierający elementy tablicy oddzielone przecinkami
+     * opis: Zwraca posortowaną tablicę w postaci napisu
+     * autor: 78901234567
+     * ****************************************************/
     @Override
     public String toString() {
-        return  Arrays.toString(posortowana);
+        return Arrays.toString(posortowana);
     }
 }
